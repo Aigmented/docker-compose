8124a6914d2d8cc4c16a59a9bfdb6b69c6b83851
Sebastian Sztemberg <sztembus@gmail.com>
2025-01-24 15:36:29 +0100

    Update run-tls.sh

--- tls/run-tls.sh
+++ tls/run-tls.sh
@@ -7,10 +7,3 @@ mkdir -p .pki
 
 # Run container to name volume and copy out CA certificate
 docker run --rm -v temporal_tls_pki:/pki -v ${PWD}/.pki:/pki-out temporal_tls:test
-
-# Build extra layers which copy in CA certificate to local trust store
-# Allows for not having to disable host verification on TLS connections
-COMPOSE_PROJECT_NAME=tls_test docker-compose -f docker-compose-tls.yml build --no-cache
-
-# Run example docker-compose environment with elasticsearch and postgresql protected with TLS
-COMPOSE_PROJECT_NAME=tls_test docker-compose -f docker-compose-tls.yml up
